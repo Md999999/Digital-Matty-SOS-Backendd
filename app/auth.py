f5c30eac0e3320b406ecaf2519a426ad3c450701
Md999999 <160726056+Md999999@users.noreply.github.com>
2025-05-24 19:44:50 +0100

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import jwt, JWTError
 from datetime import datetime, timedelta
+from passlib.context import CryptContext
 from app.utils import success_response,error_response
 
 router = APIRouter()
@@ -9,6 +10,14 @@ SECRET_KEY = "mysecret"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+pwd_context= CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(plain, hashed):
+    return pwd_context.verify(plain, hashed)
 
 users = {}
 
@@ -30,16 +39,16 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
 def register(form: OAuth2PasswordRequestForm = Depends()):
     if form.username in users:
         raise HTTPException(status_code=400, detail=error_response("User exists"))
-    users[form.username] = form.password
+    users[form.username] = hash_password(form.password)
     return success_response("User registered") 
 
 @router.post("/login")
 def login(form: OAuth2PasswordRequestForm = Depends()):
     user = users.get(form.username)
-    if not user or user != form.password:
+    if not user or  not verify_password(form.password, user):
         raise HTTPException(status_code=400, detail=error_response("Wrong credentials"))
     token = create_token(form.username)
-    return success_response("Login Successful",{"access_token": token, "token_type": "bearer"})
+    return success_response("Login Ssccessful",{"access_token": token, "token_type": "bearer"})
 
 @router.get("/protected")
 def protected(user: str = Depends(get_current_user)):
