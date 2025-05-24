705a04af716043e41b430be29e7b3d49696a6e24
Md999999 <160726056+Md999999@users.noreply.github.com>
2025-05-24 11:52:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from app import auth, routes
 
-app = FastAPI()
+app = FastAPI(
+    title="SOS Alert System",
+    description="Backend for emergency contacts and alerts",
+    version="1.0.0"
+)
 
 app.include_router(auth.router)
 app.include_router(routes.router)
